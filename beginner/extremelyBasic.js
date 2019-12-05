eaeb7de63978dfd78db863a7dfecee01fc574937
Murilo Ignacio <murilohsignacio@gmail.com>
2019-12-05 12:32:23 -0300

    completing the first exercise

---
+++ beginner/extremelyBasic.js
@@ -0,0 +1,5 @@
+// Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X
+let a = Number(prompt("Type the first number."));
+let b = Number(prompt("Type the second number."));
+let x = a + b;
+console.log(`X = ${x}`);
